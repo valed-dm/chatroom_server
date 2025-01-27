dd65f670789d76fb0b245ff37188ca2b30abfa7e
valed-dm <dmvaled@gmail.com>
2025-01-27 12:51:12 +0300

    wss connection tester created.

---
+++ tests/wss_test.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+import ssl
+
+import websockets
+
+
+async def wss_test():
+    """Test wss server"""
+    uri = "wss://localhost:8765"
+
+    # Create an SSL context that skips certificate verification
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+    try:
+        async with websockets.connect(uri, ssl=ssl_context) as websocket:
+            await websocket.send("Hello, Server!")
+            response = await websocket.recv()
+            logging.info(response)
+    except Exception as e:
+        exc_msg = f"An error occurred: {e}"
+        logging.exception(exc_msg)
+
+
+if __name__ == "__main__":
+    asyncio.run(wss_test())
