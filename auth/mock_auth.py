cff59bdf01bfc750a288aad2f19f9afb3b2da351
valed-dm <dmvaled@gmail.com>
2025-02-01 10:28:38 +0300

    mock_auth.py refactored for tests.

--- auth/mock_auth.py
+++ auth/mock_auth.py
@@ -1,6 +1,7 @@
 class MockAuth:
     def __init__(self, valid_token=None):
         self.valid_token = valid_token
+        self.not_valid_token = "not_valid_token"  # noqa: S105
 
     def is_authorized(self, request):
         auth_header = request.headers.get("Authorization")
@@ -8,4 +9,6 @@ class MockAuth:
             return False
 
         token = auth_header.split("Bearer ")[1].strip()
+        if token == self.not_valid_token:
+            return False
         return token is not self.valid_token
