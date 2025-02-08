90c531196eeeb28eaaa24f7e621caf029ad8e433
valed-dm <dmvaled@gmail.com>
2025-02-08 11:58:34 +0300

    message_rate.py logging refactored
    to return client_token info.

--- handlers/wss_srv/client/message_rate.py
+++ handlers/wss_srv/client/message_rate.py
@@ -2,9 +2,12 @@ import json
 import logging
 
 
-async def handle_message_rate_limit(connection, client_ip):
+async def handle_message_rate_limit(connection, client_token, client_ip):
     """Handle message rate limiting."""
-    logging.warning(f"Client {client_ip} is sending messages too fast. Disconnecting.")
+    logging.warning(
+        f"Client {client_token}:{client_ip} "
+        f"is sending messages too fast. Disconnecting.",
+    )
     await connection.send(json.dumps(
         {"type": "system", "content": "Rate limit exceeded. Disconnecting."}),
     )
